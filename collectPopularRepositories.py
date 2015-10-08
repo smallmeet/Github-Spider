b690718df41e96450fe0d7079c09ac3ec9755af9
Ashutosh Purushottam <sonu.puru1807@gmail.com>
2015-10-08 16:51:45 +0530

    incomplete: collect 1000 most starred repositories of github

---
+++ collectPopularRepositories.py
@@ -0,0 +1,64 @@
+"""
+Script for collecting most popular repositories on github (in terms of number of
+times the repositories have been starred, forked and number of commits).
+The repository full name is being extracted here
+"""
+import requests
+import json
+import time
+
+# enter user name and password for making API request with permission
+username = raw_input("Enter username: ")
+password = raw_input("Enter password: ")
+
+
+# open file in append mode
+try:
+    fhand = open("repo_list.txt", "a")
+except:
+    print "Could not open file. Check file name in code."
+
+repo_count = 0
+# Example hit: https://api.github.com/search/repositories?q=stars%3A%3E0&sort=stars&page=2&per_page=100
+
+def fetch_repos_stars():
+    """
+    Fetches 1000 most starred repository's full name and writes it to the external file
+    (Only the first 1000 search results are available.)
+    """
+    global repo_count
+
+    # we will make iternations for the first 20 pages with each page consisting of 100 search results
+    for page_number in range(1, 11):
+        req_url_stars = "https://api.github.com/search/repositories?q=stars%3A%3E0&sort=stars&"\
+            + "page=" + str(page_number)+ "&per_page=100"+"&format=json"
+        connect_timeout = 60.0
+
+        try:
+            response = requests.get(req_url_stars, auth=(username, password), timeout=connect_timeout)
+        except requests.exceptions.Timeout as e:
+            print "Server seems to be too slow in responding. So exiting!", e
+            break
+        except requests.exceptions.RequestException as e:
+            print "Some error:", e
+            break
+
+        if response.status_code != 200:
+            print "Invalid response from server. Exiting!", str(response.status_code)
+            break
+
+        # parse response into a dictioanry
+        response_dictionary = json.loads(response.text or response.content)
+        # extract array of repositories from the dictionary
+        repos_array = response_dictionary["items"]
+        for repo in repos_array:
+            repo_count += 1
+            repo_full_name = repo["full_name"]
+            fhand.write(repo_full_name + "\n")
+            print "Adding repository number " + str(repo_count) + ": " + repo_full_name
+
+
+
+fetch_repos_stars()
+fhand.close()
+
